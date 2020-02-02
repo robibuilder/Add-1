764db39058a79b447a7e73a06c073c64b5ac42c0
Jacob Robinett <jacob.robinett@ou.edu>
2020-02-02 13:44:07 -0600

    Initial Commit

---
+++ Add 1Tests/Add_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Add_1Tests.swift
+//  Add 1Tests
+//
+//  Created by Jacob Robinett on 2/2/20.
+//  Copyright © 2020 Jacob Robinett. All rights reserved.
+//
+
+import XCTest
+@testable import Add_1
+
+class Add_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
